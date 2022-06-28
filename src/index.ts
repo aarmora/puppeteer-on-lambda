d778a5e8c0f4b7f872bb14924b3bb80621390b8d
Jordan Hansen <jbhansen84@gmail.com>
2022-06-28 05:54:12 -0600

    update function configuration to change ip

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import chromium from 'chrome-aws-lambda';
+import aws from 'aws-sdk';
+
+const lambda = new aws.Lambda();
 
 export async function handler(event) {
-    console.log('Received', event);
 
-    console.log('event query param', event.queryStringParameters?.pizza);
+    await run();
 
-    const title = await run();
+    await lambda.updateFunctionConfiguration({
+        FunctionName: 'puppeteer-on-lambda',
+        Environment: {
+            Variables: {}
+        }
+    }).promise();
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify({ title: title })
+        body: ''
     };
 
     return response;
@@ -26,13 +33,10 @@ export async function run() {
 
     const page = await browser.newPage();
 
-    console.log('Before going to jswebscrapingguy');
-    await page.goto('https://javascriptwebscrapingguy.com/');
-    let title = await page.$eval('title', element => element.textContent);
+    await page.goto('https://lumtest.com/myip.json');
+    const body = await page.$eval('body', element => element.textContent);
 
-    console.log('First title', title);
+    console.log('body', body);
 
     await browser.close();
-
-    return title;
 }
